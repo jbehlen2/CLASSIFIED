7d2f8018b3b482d286af7092c09c13157b1847b7
joshuabehlen <behlenjosh@gmail.com>
2017-10-25 16:52:00 -0500

    Add files via upload

---
+++ alu16bit.vhdl
@@ -0,0 +1,32 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+ENTITY alu16bit IS
+	PORT(
+		A,B :IN std_logic_vector(15 downto 0);
+		alu_op :IN std_logic_vector(1 downto 0);
+		A_inv,B_inv :IN std_logic;
+		ALU_out :OUT std_logic_vector(15 downto 0);
+		N,C,Z,V :OUT std_logic
+	);
+END alu16bit;
+
+ARCHITECTURE behavior of alu16bit IS
+	COMPONENT ripCarryAdder
+		PORT(
+			A,B :IN std_logic_vector(15 downto 0);
+			Cin :IN std_logic;
+			F: OUT std_logic_vector(15 downto 0);
+			C14,C15 :OUT std_logic
+		);
+	END COMPONENT;
+	COMPONENT flags
+		PORT(
+			S :IN std_logic_vector(15 downto 0);
+			C14,C15 :IN std_logic;
+			N,C,Z,V :OUT std_logic
+		);
+	END COMPONENT;
+BEGIN
+	
+END behavior;
